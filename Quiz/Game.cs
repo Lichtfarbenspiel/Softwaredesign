5aab8e17e121688a8079aad80a857d6f6e866be0
lichtfarbenspiel <s.belua.d@gmail.com>
2019-12-03 10:00:04 +0100

    Tree Diagram

--- Quiz/Game.cs
+++ Quiz/Game.cs
@@ -54,10 +54,12 @@ namespace Softwaredesign.Quiz
         void PlayQuiz()
         {
             DefaultQuiz();
-            var rnd = new Random();
-            var rndList = quizzes.OrderBy(item => rnd.Next());
+            Random rnd = new Random();
+            // var rndList = quizzes.OrderBy(item => rnd.Next());
 
-            var currentQuiz = rndList.ElementAt(0);
+            int number  = rnd.Next(0, quizzes.Count);
+
+            var currentQuiz = quizzes.ElementAt(number);
             currentQuiz.Display();
 
             Write(">");
@@ -71,12 +73,13 @@ namespace Softwaredesign.Quiz
             else{
                 WriteLine(MsgWrongAnswer);
             }
+            quizzes.Remove(currentQuiz);
             quizCount +=1;
             Menu();
         }
 
         void AddUserQuiz(){
-            AddQuiz quiz = new AddQuiz();
+        
             WriteLine("Please select a quiz type to add:");
             WriteLine("1: Guess Quiz");
             WriteLine("2: Text Quiz");
@@ -88,41 +91,183 @@ namespace Softwaredesign.Quiz
 
             int selection = Read();
 
+            // if(selection == 1)
+            //     AddQuizGuess();
+            // else if(selection == 2)
+            //     AddQuizText();
+            // else if(selection == 3)
+            //     AddQuizText();
+            // else if(selection == 4)
+            //     AddQuizSingleChoice();
+            // else if(selection == 5)
+            //     AddQuizMultipleChoice();
+            // else if(selection == 6)
+            //     Menu();
+
             switch(selection){
                 case 1:
-                    quiz.AddQuizGuess();
+                    AddQuizGuess();
                     break;
                 case 2:
-                    quiz.AddQuizText();
-                    
+                    AddQuizText();                    
                     break;
                 case 3:
-                    quiz.AddQuizTrueFalse();
-                    
+                    AddQuizTrueFalse();                    
                     break;
                 case 4:
-                    quiz.AddQuizSingleChoice();
-                    
+                    AddQuizSingleChoice();                    
                     break;
                 case 5:
-                    quiz.AddQuizMultipleChoice();
-                    
-                break;
+                    AddQuizMultipleChoice();                    
+                    break;
                 case 6:
                     Menu();
-                    break;
-                
+                break;
             }
 
         }
 
         void DefaultQuiz(){
-            QuizText quizText = new QuizText("What answeres in all languages? Talks without a mouth? Listens without ears?", new Answer("Echo", true));
-            quizzes.Add(quizText);
-            QuizGuess quizGuess = new QuizGuess("How many Letters has the Hawaiian alphabet?", 0.3f, 12f);
-            quizzes.Add(quizGuess);
-            QuizTrueFalse quizTrueFalse = new QuizTrueFalse("The Vatican State is not a democracy but the only dictatorship in Europe.", true);
-            quizzes.Add(quizTrueFalse);
+            // quizzes.Add(
+            //     new QuizText("What answeres in all languages? Talks without a mouth? Listens without ears?", new Answer("Echo", true))
+            // );
+            // quizzes.Add(
+            //     new QuizGuess("How many Letters has the Hawaiian alphabet?", 0.3f, 12f)
+            // );
+            // quizzes.Add(
+            //     new QuizTrueFalse("The Vatican State is not a democracy but the only dictatorship in Europe.", true)
+            // );
+            quizzes.Add(new QuizMultipleChoice("Who is a Professor at Hogwarts?", new Answer[6]{
+                new Answer("Prof. Dumbledore", true), new Answer("Prof. McDonalds", false), new Answer("Prof. MacGonagall", true), new Answer("Prof. Hagrid", false), new Answer("Prof. Umbridge", true), new Answer("Prof. Sniper", false)
+            }));
+            
+        }
+
+
+        string instructionsQuestion = "Please enter the question.";
+        void AddQuizText()
+        {
+            WriteLine(instructionsQuestion);
+            Write(">");
+            string question = ReadLine();
+            WriteLine("Please enter the answer.");
+            Write(">");
+            string userAnswer = ReadLine();
+
+            Answer answer = new Answer(userAnswer, true);
+            quizzes.Add(new QuizText(question, answer));
+            
+            Menu();
+        }
+
+        void AddQuizTrueFalse()
+        {
+            bool isTrue = false;
+            WriteLine(instructionsQuestion);
+            Write(">");
+            string question = ReadLine();
+            WriteLine("Please enter 'T' if the question is correct. Enter 'F' if it is incorrect.");
+            Write(">");
+            string input = ReadLine();
+
+            if(input == "T" || input == "t")
+                isTrue = true;
+            else if(input == "F" || input == "f")
+                isTrue = false;
+
+            quizzes.Add(new QuizTrueFalse(question, isTrue));
+            Menu();
+        }
+
+        void AddQuizGuess()
+        {
+            WriteLine(instructionsQuestion);
+            WriteLine(">");
+            string question = ReadLine();
+
+            WriteLine("Please enter the correct answer as a number. It may be a float number seperated by a dot.");
+            Write(">");
+            float answer = float.Parse(ReadLine());
+
+            WriteLine("Please enter the 'percentage' of the valid tolarance from 0 to 1. E.g. 0.2 for a tolerance of 20%.");
+            Write(">");
+            float tolerance = float.Parse(ReadLine());
+
+            new QuizGuess(question, tolerance, answer);
+            Menu();
+        }
+
+        void AddQuizMultipleChoice()
+        {
+            Answer[] answers = new Answer[6];
+            string correctAnswer = "";
+            string wrongAnswer = "";
+            int count = 0;
+
+            WriteLine(instructionsQuestion);
+            WriteLine(">");
+            string question = ReadLine();
+
+            WriteLine("Please enter the correct answer");
+            Write(">");
+            correctAnswer = ReadLine();
+            while(correctAnswer != ""){
+                for(int i = 0; i <= answers.Length - 1; i++){
+                    answers[i].text = correctAnswer;
+                    answers[i].isTrue = true;
+                    Write(">");
+                    correctAnswer = ReadLine();
+                    count ++;
+                }
+            }
+           
+            WriteLine("Please enter up to 5 more answers which aren't correct.");
+            Write(">");
+            wrongAnswer = ReadLine();
+
+            while(wrongAnswer != ""){
+                for(int i = count + 1; i <= answers.Length - count; i++){
+                    answers[i].text = wrongAnswer;
+                    answers[i].isTrue = false;
+                    Write(">");
+                    wrongAnswer = ReadLine();
+                }
+            }
+
+            quizzes.Add(new QuizMultipleChoice(question, answers));
+            Menu();
+
+        }
+        void AddQuizSingleChoice()
+        {
+            Answer[] answers = new Answer[6];
+            string answer = "";
+
+            WriteLine(instructionsQuestion);
+            WriteLine(">");
+            string question = ReadLine();
+
+            WriteLine("Please enter the correct answer");
+            Write(">");
+            string correctAnswer = ReadLine();
+            answers[0].text = correctAnswer;
+            answers[0].isTrue = true;
+
+            WriteLine("Please enter up to 5 more answers which aren't correct.");
+            Write(">");
+            answer = ReadLine();
+
+            while(answer != ""){
+                for(int i = 1; i <= answers.Length -1; i++){
+                    answers[i].text = answer;
+                    answers[i].isTrue = false;
+                    Write(">");
+                    answer = ReadLine();
+                }
+            }
+
+            quizzes.Add(new QuizSingleChoice(question, answers));
+            Menu();
         }
 
     }
--- Quiz/QuizMultipleChoice.cs
+++ Quiz/QuizMultipleChoice.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace Softwaredesign.Quiz
@@ -8,6 +9,7 @@ namespace Softwaredesign.Quiz
 
         Answer[] answers;
         new string instructions = "'Multiple Choice' Please enter all the numbers separated only by a ',' (komma) of those answers you believe are correct. Example: '1,3,4,5' When finished press the 'enter' key.";
+        bool[] correctAnswers;
 
         public QuizMultipleChoice(string question, Answer[] answers) : base(question)
         {
@@ -17,12 +19,16 @@ namespace Softwaredesign.Quiz
         public override void Display()
         {   
             int count = 1;
+            // Random rnd = new Random();
+            // int number  = rnd.Next(0, answers.Length-1);
+            
 
             WriteLine(instructions);
             WriteLine(question);
            
             for(int i = 0; i < answers.Length; i++){
-                WriteLine(count + ": " + answers[i]);
+                WriteLine(count + ": " + answers[i].text);
+                bool[] correctAnswers = {answers[i].isTrue};
                 count ++;
             }
 
@@ -33,18 +39,21 @@ namespace Softwaredesign.Quiz
             bool selection = false;
 
             int[] number = Array.ConvertAll(userInput.Split(','), int.Parse);
-;
+    
 
             for(int i = 1; i < answers.Length; i++){
                 
-                if(number[i-1] == i && answers[i].isTrue)
+                if(number[i] == i && answers[i].isTrue)
                     selection = true;
-                else if(number[i-1] == i && !answers[i].isTrue)
+                else if(number[i] == i + 1 && !answers[i].isTrue)
                     selection = false;
                 else 
                     WriteLine(MsgWrongInput);
                     selection = false;
             }
+            
+
+
             return selection;
         }
 
